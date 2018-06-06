daea5a31e43ec661a8dbe97fe6027d1fe8e396a7
Celio Murakami <celio@invictos.com.br>
2018-06-06 14:38:51 -0400

    Apos merge faltou alteração antiga Bancoob

--- Fontes/ACBrBoleto/ACBrBancoBancoob.pas
+++ Fontes/ACBrBoleto/ACBrBancoBancoob.pas
@@ -676,13 +676,16 @@ begin
          ValorRecebido        := StrToFloatDef(Copy(Linha,254,13),0)/100;
          NossoNumero          := copy( Copy(Linha,63,11),Length( Copy(Linha,63,11) )-TamanhoMaximoNossoNum+1  ,TamanhoMaximoNossoNum);
          Carteira             := Copy(Linha,86,3);
-         ValorDespesaCobranca := StrToFloatDef(Copy(Linha,182,7),0)/100;
-         ValorOutrasDespesas  := StrToFloatDef(Copy(Linha,189,13),0)/100;
+         ValorDespesaCobranca := StrToFloatDef(Copy(Linha,182,7),0)/100;ValorOutrasDespesas  := StrToFloatDef(Copy(Linha,189,13),0)/100;
 
+         { invictos }
          if StrToIntDef(Copy(Linha,176,6),0) <> 0 then
             DataCredito:= StringToDateTimeDef( Copy(Linha,176,2)+'/'+
                                                Copy(Linha,178,2)+'/'+
-                                               Copy(Linha,180,2),0, 'DD/MM/YY' );
+                                               Copy(Linha,180,2),0, 'DD/MM/YY' )
+         else DataCredito := StringToDateTimeDef( Copy(Linha,111,2)+'/'+
+                                               Copy(Linha,113,2)+'/'+
+                                               Copy(Linha,115,2),0, 'DD/MM/YY' );
       end;
    end;
 
